680b011d17e492098ae7c5bf0ccb3cf87916d4c8
dnkim419 <dnkim419@gmail.com>
2023-11-03 15:44:46 +0900

    Feat: Feed-Forward

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# Sinusoidal position representations
+# sinusoidal position representations
 def get_sinusoidal(n_seq, d_model):
   '''
   Args:
@@ -22,6 +22,28 @@ def get_sinusoidal(n_seq, d_model):
   pos_enc_table[:, 1::2] = np.cos(pos_enc_table[:, 1::2]) # odd idx
 
   return pos_enc_table
+
+
+# ffn
+class FFN(nn.Module):
+  def __init__(self, config):
+    super().__init__()
+    self.config = config
+
+    self.conv1 = nn.Conv1d(in_channels=self.config["d_model"], out_channels=self.config["d_ff"], kernel_size=1)
+    self.conv2 = nn.Conv1d(in_channels=self.config["d_ff"], out_channels=self.config["d_model"], kernel_size=1)
+    self.active = F.relu
+    self.dropout = nn.Dropout(self.config["dropout"])
+
+  # inputs: (batch, n_seq, d_model)
+  def forward(self, inputs):
+    # (batch, n_seq, d_model) -> (batch, d_model, n_seq) -> (batch, d_ff, n_seq)
+    output = self.active(self.conv1(inputs.transpose(1,2)))
+    # (batch, d_ff, n_seq) -> (batch, d_model, n_seq) -> (batch, n_seq, d_model)
+    output = self.conv2(output).transpose(1,2)
+    output = self.dropout(output)
+    # output: (batch, n_seq, d_model)
+    return output
 
 
 # encoder
